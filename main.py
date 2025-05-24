cdab7480f92949f22ccaf4202ecbf43a0ff7a63f
K7stia <k7stia@gmail.com>
2025-05-25 00:11:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ api_id = os.getenv("API_ID")
 api_hash = os.getenv("API_HASH")
 
 if not api_id or not api_hash:
-    raise RuntimeError("❌ Змінні API_ID або API_HASH не задані в Railway Variables")
+    raise RuntimeError("❌ API_ID або API_HASH не задані!")
 
 with TelegramClient(StringSession(), int(api_id), api_hash) as client:
-    print("\n🔐 Ось твій TELETHON_SESSION:")
+    print("\n🔐 TELETHON_SESSION:")
     print(client.session.save())
